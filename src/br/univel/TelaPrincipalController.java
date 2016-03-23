b39da14213a2867fdd94d9bdfd36ab751f33034e
Jhonatan Mattana <jhonatan-mattana@hotmail.com>
2016-03-22 21:09:53 -0300

    Aula Scene Builder com JavaFX

---
+++ src/br/univel/TelaPrincipalController.java
@@ -0,0 +1,23 @@
+package br.univel;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class TelaPrincipalController {
+	@FXML
+    private Button botao;
+
+    @FXML
+    private TextField campo;
+
+    @FXML
+    private Label nome;
+
+    @FXML
+    void executar(ActionEvent event) {
+    	System.out.println(campo.getText());
+    }
+}
